b54e00e06b839c9c7ad73a15c3b5103fdbe89361
Sebastian Fuhrhop <s.fuhrhop@live.de>
2018-04-10 21:07:23 +0200

    add default node factory

--- Tangle.Net/Tangle.Net.Mam/Services/CurlMamFactory.cs
+++ Tangle.Net/Tangle.Net.Mam/Services/CurlMamFactory.cs
@@ -1,8 +1,6 @@
 ﻿namespace Tangle.Net.Mam.Services
 {
-  using System;
   using System.Collections.Generic;
-  using System.Diagnostics.CodeAnalysis;
   using System.Linq;
 
   using Tangle.Net.Cryptography;
