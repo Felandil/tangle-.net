3cdd98166eb59efdd7f593fb68a84b7343f03abf
Sebastian Fuhrhop <s.fuhrhop@hotmail.de>
2021-05-10 23:15:34 +0200

    add payload signing/verification

---
+++ Tangle.Net.Tests/Crypto/Ed25519Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tangle.Net.Crypto;
+using Tangle.Net.Entity.Ed25519;
+using Tangle.Net.Utils;
+
+namespace Tangle.Net.Tests.Crypto
+{
+  [TestClass]
+  public class Ed25519Test
+  {
+    [TestMethod]
+    public void TestSigantureGeneration()
+    {
+      var expected =
+        "0acae5d099c82549ead3989cd49b671a72b33118150d00bb699ce31238b548c862d06e675243cbc4195a8239ef593530733142961ac08620c0d8855758581908";
+
+      var seed = Ed25519Seed.FromMnemonic("witch collapse practice feed shame open despair creek road again ice least");
+      var actual = Ed25519.Sign("Sign this!".Utf8ToBytes(), seed);
+
+      Assert.AreEqual(expected, actual.Signature);
+    }
+
+    [TestMethod]
+    public void TestSignatureVerificationShouldSucceed()
+    {
+      var seed = Ed25519Seed.FromMnemonic("witch collapse practice feed shame open despair creek road again ice least");
+      var payload = "Sign this!".Utf8ToBytes();
+      var signature = Ed25519.Sign(payload, seed);
+
+      Assert.IsTrue(Ed25519.Verify(payload, signature));
+    }
+
+    [TestMethod]
+    public void TestSignatureVerificationShouldFail()
+    {
+      var seed = Ed25519Seed.FromMnemonic("witch collapse practice feed shame open despair creek road again ice least");
+      var signature = Ed25519.Sign("Sign this!".Utf8ToBytes(), seed);
+
+      Assert.IsFalse(Ed25519.Verify("Something".Utf8ToBytes(), signature));
+    }
+  }
+}
---
+++ Tangle.Net/Crypto/Ed25519.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Tangle.Net.Entity.Ed25519;
+using Tangle.Net.Utils;
+
+namespace Tangle.Net.Crypto
+{
+  public static class Ed25519
+  {
+    public static Ed25519Signature Sign(byte[] payload, Ed25519Seed seed)
+    {
+      var ed25519 = new Rebex.Security.Cryptography.Ed25519();
+      ed25519.FromSeed(seed.SecretKey.Take(32).ToArray());
+
+      var signature = ed25519.SignMessage(payload);
+
+      return new Ed25519Signature
+      {
+        PublicKey = seed.KeyPair.PublicKey.ToHex(),
+        Signature = signature.ToHex()
+      };
+    }
+
+    public static bool Verify(byte[] payload, Ed25519Signature signature)
+    {
+      var ed25519 = new Rebex.Security.Cryptography.Ed25519();
+      ed25519.FromPublicKey(signature.PublicKey.HexToBytes());
+
+      return ed25519.VerifyMessage(payload, signature.Signature.HexToBytes());
+    }
+  }
+}
